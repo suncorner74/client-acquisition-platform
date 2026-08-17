f4e2bb2efeb58b45506c3699f22be50e2bcfdc29
Suraj Gupta <35479827+suncorner74@users.noreply.github.com>
2026-08-17 22:19:43 +0530

    Add admin role authorization middleware

---
+++ server/src/middleware/adminMiddleware.js
@@ -0,0 +1,12 @@
+const requireAdmin = (req, res, next) => {
+  if (!req.user || req.user.role !== 'admin') {
+    return res.status(403).json({
+      success: false,
+      message: 'Admin privileges required'
+    });
+  }
+
+  next();
+};
+
+module.exports = { requireAdmin };
